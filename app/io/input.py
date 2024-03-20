67ba0d67d18e9690b20530f3db93948769511fc1
Dasha Filozop <filozopdasha@Dashas-MBP.Dlink>
2024-03-20 20:54:24 +0200

    written input and output functions

---
+++ app/io/input.py
@@ -0,0 +1,9 @@
+def input_from_console():
+    return 0
+
+
+def read_file_builtin():
+    return 0
+
+def read_file_with_pandas():
+    return 0
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def output_to_console():
+    return 0
+
+
+def write_to_file_builtin():
+    return 0
+
+
+def write_to_file_with_pandas():
+    return 0
